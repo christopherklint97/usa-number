5a26808dd16c2cb296451929f171a40bdded73b4
Christopher Klint <christopher.klint@gmail.com>
2022-03-20 22:06:36 +0100

    feat: add post sms route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import "dotenv/config";
 import express from "express";
-import createTwilioClient from "twilio";
+import createTwilioClient, { twiml } from "twilio";
 
 const app = express();
 const PORT = 8000;
@@ -7,6 +8,16 @@ const PORT = 8000;
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioClient = createTwilioClient(accountSid, authToken);
+
+app.post("/sms", (req, res) => {
+  const { MessagingResponse } = twiml;
+  const messagingResponse = new MessagingResponse();
+
+  messagingResponse.message("The Robots are coming! Head for the hills!");
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(messagingResponse.toString());
+});
 
 app.get("/", (req, res) => {
   twilioClient.messages
